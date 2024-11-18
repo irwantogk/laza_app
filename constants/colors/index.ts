8d02c015490472d60ff8069e71f5366e8b520aad
irwantogk <irwantowibowo1992@gmail.com>
2024-11-19 06:54:07 +0700

    setting constant variable

---
+++ constants/colors/index.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  blue: "#9775FA",
+  orange: "#FF7043",
+  white: "#FFFFFF",
+  black: "#1D1E20",
+  gray: "#8F959E",
+  dark1: "#1B262C",
+  dark2: "#222E34",
+  dark3: "#29363D",
+};
---
+++ constants/fonts/index.ts
@@ -0,0 +1,21 @@
+export const fonts = {
+  weight: {
+    bold: "bold",
+  },
+  size: {
+    header: {
+      lg: 34,
+      medium: 28,
+      sm: 22,
+    },
+    paragraf: {
+      p34: 34,
+      p28: 28,
+      p22: 22,
+      p17: 17,
+      p15: 15,
+      p13: 13,
+      p11: 11,
+    },
+  },
+};
